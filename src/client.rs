cad4934571d40aa327768c00d4e97eeeacc1393e
John T. Wodder II <git@varonathe.org>
2024-04-28 12:24:59 -0400

    Client::list_directory()

--- src/client.rs
+++ src/client.rs
@@ -1,4 +1,7 @@
 use crate::types::DirectoryListing;
+use crate::xml::parse_propfind_response;
+use indoc::indoc;
+use reqwest::Method;
 use thiserror::Error;
 use url::Url;
 
@@ -11,10 +14,22 @@ static USER_AGENT: &str = concat!(
     ")",
 );
 
+static REQUEST_CONTENT_TYPE: &str = "text/xml; utf-8";
+
+static REQUEST_BODY: &str = indoc! {r#"
+    <?xml version="1.0" encoding="utf-8"?>
+    <propfind xmlns="DAV:">
+        <prop>
+            <resourcetype/>
+        </prop>
+    </propfind>
+"#};
+
 #[derive(Clone, Debug)]
 pub(crate) struct Client {
     base_url: Url,
     inner: reqwest::Client,
+    propfind: Method,
 }
 
 impl Client {
@@ -24,12 +39,31 @@ impl Client {
             .redirect(reqwest::redirect::Policy::none())
             .build()
             .map_err(BuildClientError)?;
-        Ok(Client { inner, base_url })
+        Ok(Client {
+            inner,
+            base_url,
+            propfind: "PROPFIND"
+                .parse()
+                .expect(r#""PROPFIND" should be valid HTTP method"#),
+        })
     }
 
     // Assume `url` has `base_url` as a prefix
     pub(crate) async fn list_directory(&self, url: Url) -> anyhow::Result<DirectoryListing<Url>> {
-        todo!()
+        let resp = self
+            .inner
+            .request(self.propfind.clone(), url.clone())
+            .header(reqwest::header::CONTENT_TYPE, REQUEST_CONTENT_TYPE)
+            .header("Depth", "1")
+            .body(REQUEST_BODY)
+            .send()
+            .await?
+            .error_for_status()?
+            .text_with_charset("utf-8")
+            .await?;
+        let mut dl = parse_propfind_response(&resp)?.paths_to_urls(&self.base_url);
+        dl.directories.retain(|u| !is_collection_url(&url, u));
+        Ok(dl)
     }
 
     // Assume `url` has `base_url` as a prefix
@@ -51,3 +85,7 @@ impl Client {
 #[derive(Debug, Error)]
 #[error("failed to initialize HTTP client")]
 pub(crate) struct BuildClientError(#[source] reqwest::Error);
+
+fn is_collection_url(colurl: &Url, url: &Url) -> bool {
+    colurl.as_str().trim_end_matches('/') == url.as_str().trim_end_matches('/')
+}
