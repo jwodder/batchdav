f88000301b4065490391b8a8b90688b89701a776
John T. Wodder II <git@varonathe.org>
2024-04-28 15:48:40 -0400

    Lex XML responses into a Vec<Token>

--- src/xml.rs
+++ src/xml.rs
@@ -1,11 +1,99 @@
+/*
+For reference: Relevant DTD fragments from
+<http://www.webdav.org/specs/rfc4918.html#xml.element.definitions>:
+
+    <!ELEMENT multistatus (response*, responsedescription?)>
+    <!ELEMENT response (href, ((href*, status)|(propstat+)),
+                        error?, responsedescription?, location?)>
+    <!ELEMENT href (#PCDATA)>
+    <!ELEMENT propstat (prop, status, error?, responsedescription?)>
+    <!ELEMENT prop ANY>
+    <!ELEMENT status (#PCDATA)>
+    <!ELEMENT error ANY>
+    <!ELEMENT responsedescription (#PCDATA)>
+    <!ELEMENT location (href)>
+*/
+
 use crate::types::DirectoryListing;
-use bytes::Bytes;
+use bytes::{Buf, Bytes};
+use thiserror::Error;
+use xml::reader::{Error as XmlError, ParserConfig2, XmlEvent};
+
+/// The XML namespace for standard WebDAV elements
+static DAV_XMLNS: &str = "DAV:";
 
 pub(crate) fn parse_multistatus(
     blob: Bytes,
     charset: Option<String>,
-) -> anyhow::Result<DirectoryListing<String>> {
+) -> Result<DirectoryListing<String>, FromXmlError> {
+    parse(tokenize(blob, charset)?)
+}
+
+#[derive(Clone, Debug, Eq, Hash, Ord, PartialEq, PartialOrd)]
+enum Token {
+    OpenDav(String),
+    CloseDav(String),
+    OpenForeign(String),
+    CloseForeign(String),
+    Text(String),
+}
+
+impl Token {
+    fn open(name: String, namespace: Option<String>) -> Token {
+        if namespace.map_or(true, |ns| ns == DAV_XMLNS) {
+            Token::OpenDav(name)
+        } else {
+            Token::OpenForeign(name)
+        }
+    }
+
+    fn close(name: String, namespace: Option<String>) -> Token {
+        if namespace.map_or(true, |ns| ns == DAV_XMLNS) {
+            Token::CloseDav(name)
+        } else {
+            Token::CloseForeign(name)
+        }
+    }
+}
+
+fn tokenize(blob: Bytes, charset: Option<String>) -> Result<Vec<Token>, XmlTokenizeError> {
+    let encoding = charset.and_then(|cs| cs.parse::<xml::Encoding>().ok());
+    let reader = ParserConfig2::new()
+        .ignore_invalid_encoding_declarations(false)
+        .override_encoding(encoding)
+        .allow_multiple_root_elements(false)
+        .trim_whitespace(true)
+        .create_reader(blob.reader());
+    let mut tokens = Vec::new();
+    for event in reader {
+        use XmlEvent::*;
+        match event? {
+            StartElement { name, .. } => tokens.push(Token::open(name.local_name, name.namespace)),
+            EndElement { name, .. } => tokens.push(Token::close(name.local_name, name.namespace)),
+            CData(s) | Characters(s) => tokens.push(Token::Text(s)),
+            StartDocument { .. } | EndDocument | Comment(..) | Whitespace(..) => (),
+            ProcessingInstruction { .. } => return Err(XmlTokenizeError::ProcessingInstruction),
+        }
+    }
+    Ok(tokens)
+}
+
+fn parse(tokens: Vec<Token>) -> Result<DirectoryListing<String>, FromXmlError> {
     todo!()
+}
+
+#[derive(Clone, Debug, Error, Eq, PartialEq)]
+pub(crate) enum FromXmlError {
+    #[error(transparent)]
+    Tokenize(#[from] XmlTokenizeError),
+}
+
+#[derive(Clone, Debug, Error, Eq, PartialEq)]
+pub(crate) enum XmlTokenizeError {
+    #[error("error tokenizing XML")]
+    Xml(#[from] XmlError),
+    #[error("unexpected XML processing instruction encountered")]
+    ProcessingInstruction,
 }
 
 #[cfg(test)]
